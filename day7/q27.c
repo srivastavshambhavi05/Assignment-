4b5ccec334270dff6f5ef350734e81c50258c874
srivastavshambhavi05 <srivastavshambhavi268@gmail.com>
2026-06-11 21:46:45 +0530

    day-7 uploaded

---
+++ day7/q27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int sod(int);
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    printf("The sum of edigit =%d" ,sod(n));
+    return 0;
+}
+int sod(int n)
+{
+    if(n==0)
+    return 0;
+    else
+    return(n%10+sod(n/10));
+
+}
+    
